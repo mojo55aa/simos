4881b3e131e44ad259c5b74abb0303b42b8c0536
mojo55aa <690283034@qq.com>
2018-04-24 10:17:54 +0800

    commit -m "kernel PDE and PTE done"

--- mm/memory.c
+++ mm/memory.c
@@ -5,10 +5,11 @@
 #include "../include/os/bitmap.h"
 #include "../include/os/debug.h"
 #include "../include/os/stdint.h"
+#include "../include/os/string.h"
 #include "../include/asm/print.h"
 
 #define PAGE_SIZE 4096  /*页面大小4KB*/
-#define ONE_PAGE_DIR 0x400000   /*一个页目录项对应4MB内存*/
+#define ONE_PAGE_DIR_SIZE 0x400000   /*一个页目录项对应4MB内存*/
 #define TOTAL_MEM (*(uint32_t*)(0xb00))     /*物理内存容量，loader.S 中total_mem_bytes变量地址0xb00*/
 
 /*内存的一些定义*/
@@ -110,7 +111,7 @@ void mem_pool_init(uint64_t total_mem)
 }
 
 /**
- * get_kernel_pages 返回内核空间页数,同mem_pool_init的前部分
+ * get_kernel_pages 返回内核空间页数
  * @total_mem: 总内存数
  * return: 内核页表数
 */
@@ -120,51 +121,56 @@ static inline uint32_t get_kernel_pages(uint64_t total_mem)
     /*如果内存容量大于2GB，则内核固定使用1GB*/
     if(total_mem >= KERNEL_THRESHOLD)
     {
-        kernel_mem_size = KERNEL_MAX_SIZE - (KERNEL_HEAP_BASE - 0xc0000000);
+        kernel_mem_size = KERNEL_MAX_SIZE;
     }
     else/*如果小于2GB，内核和用户空间平分*/
     {
-        kernel_mem_size = total_mem / 2 - (KERNEL_HEAP_BASE - 0xc0000000);     /*kernel内存池容量需要减去已经分配出去的*/
+        kernel_mem_size = total_mem / 2; /*kernel内存池容量需要减去已经分配出去的*/
     }
 
     /*计算kernel和user内存页数*/
     return (kernel_mem_size / PAGE_SIZE);
 }
 /**
- * page_table_init 初始化内核页表，对内核空间做完整映射，替换临时页表
+ * page_table_init 初始化内核页表，对内核空间做完整映射，替换临时页表。
+ * 页目录中前768项不做映射，内核地址从0xc0000000开始
+ * 0xc0000000-0xcfffffff:0x00000000-0x3fffffff
+ * 最后一个页表指向页目录表，这样kernel最高4MB没有映射
  * @kernel_pages: 内核页数
 */
 void page_table_init(uint32_t kernel_pages)
 {
     /*页目录表的地址*/
     uint32_t *page_dir_addr = (uint32_t *)PAGE_DIR_TABLE;
-    /*初始化页目录表*/
     /*计算需要的页目录数和最后一个页目录对应页表项数*/
     uint32_t page_dir_num, last_page_table_num;
-    if ((kernel_pages * PAGE_SIZE) % ONE_PAGE_DIR == 0) /*总内存字节数除以4MB,余数为0*/
+    if ((kernel_pages * PAGE_SIZE) % ONE_PAGE_DIR_SIZE == 0) /*总内存字节数除以4MB,余数为0*/
     {
-        page_dir_num = (kernel_pages * PAGE_SIZE) / ONE_PAGE_DIR;
+        page_dir_num = (kernel_pages * PAGE_SIZE) / ONE_PAGE_DIR_SIZE;
         last_page_table_num = 0;
     }
     else    /*余数不为0，需要计算最后一个页目录项对应的页表项数*/
     {
-        page_dir_num = (kernel_pages * PAGE_SIZE) / ONE_PAGE_DIR;
+        page_dir_num = (kernel_pages * PAGE_SIZE) / ONE_PAGE_DIR_SIZE;
         /*（总内存字节数-已分配的完整内存字节数） / 一页大小*/
-        last_page_table_num = ((kernel_pages * PAGE_SIZE) - (ONE_PAGE_DIR * (kernel_pages - 1))) / PAGE_SIZE;
+        last_page_table_num = ((kernel_pages * PAGE_SIZE) - (ONE_PAGE_DIR_SIZE * (page_dir_num))) / PAGE_SIZE;
     }
 
-    uint16_t kernel_page_mark = 0x0 | PG_P_ENABLE | PG_RW_W | PG_US_S; /*kernel页表属性，存在 可写 超级用户*/
+    /*初始化页目录表*/
+    uint16_t kernel_page_mark = 0x0 | PG_P_ENABLE | PG_RW_W | PG_US_U; /*kernel页表属性，存在 可写 普通用户*/
     uint32_t i;
-    for (i = 0; i < 768; i++)    /*前168项，3GB空间不映射*/
-    {
-        page_dir_addr[i] = 0x0;
-    }
+
+    memset((void*)PAGE_DIR_TABLE, 0, 1024 * 1024);   /*页表占用的空间清零*/
+    // for (i = 0; i < 768; i++) /*前768项，3GB空间不映射*/
+    // {
+    //     page_dir_addr[i] = 0x0;
+    // }
 
     uint32_t page_table_base = (uint32_t)page_dir_addr + 0x1000;  /*内核页表基址，页目录表的下一个页框*/
     for (i = 768; i < 768 + page_dir_num; i++) /*映射内核空间*/
     {
         /*每一个页目录项*/
-        uint32_t each_page_dir = page_table_base + i + kernel_page_mark;
+        uint32_t each_page_dir = page_table_base + ((i - 768) << 12) + kernel_page_mark;
         page_dir_addr[i] = each_page_dir;
     }
 
@@ -173,9 +179,9 @@ void page_table_init(uint32_t kernel_pages)
     uint32_t memory_addr;
     for (i = 0; i < page_dir_num; i++)
     {
-        memory_addr = i * ONE_PAGE_DIR;
+        memory_addr = i * ONE_PAGE_DIR_SIZE;
         /*每次迭代的页表地址*/
-        uint32_t *page_table_addr = (uint32_t*)(page_dir_addr + 0x1000 + 0x1000 * i);
+        uint32_t *page_table_addr = (uint32_t*)(page_table_base + (i << 12));
         int j;
         for (j = 0; j < 1024; j++)
         {
@@ -183,34 +189,36 @@ void page_table_init(uint32_t kernel_pages)
             //从物理地址0x0开始
             uint32_t each_page_table = (memory_addr + j * PAGE_SIZE) + kernel_page_mark;
             page_table_addr[j] = each_page_table;
-        }
+        }    
     }
-    /**********************************************下面有问题*/
+
     /*对不是整数的页目录项单独赋值*/
-    if(last_page_table_num)
+    if (last_page_table_num)
     {
+        memory_addr += ONE_PAGE_DIR_SIZE;    /*调整物理内存地址，加4MB*/
         for (i = 0; i < last_page_table_num; i++)
         {
             uint32_t each_page_table = (memory_addr + i * PAGE_SIZE) + kernel_page_mark;
+            /*计算页表的地址*/
             *(uint32_t*)((page_table_base + (page_dir_num + 1) * 0x1000) + i * 4) = each_page_table;
         }
+        page_dir_addr[768 + page_dir_num] = (page_table_base + (page_dir_num + 1) * 0x1000) + kernel_page_mark;
     }
-    page_dir_addr[page_dir_num] = (page_table_base = (page_dir_num + 1) * 0x1000) + kernel_page_mark;
     /*让最后一个页目录项指向页目录表的基址*/
     page_dir_addr[1023] = (uint32_t)page_dir_addr + kernel_page_mark;
     /**************************************************/
-    put_str("check point 1\n");
     /*重新加载页目录表*/
-    //asm volatile("movl %0, %%cr3": :"r"(page_dir_addr));
+    asm volatile("movl %0, %%cr3": :"r"(page_dir_addr));
 }
 
 void mem_init()
 {
     put_str("memory initialization start\n");
     /*初始化内存池*/
-    mem_pool_init((uint64_t)0xc0000000);
+    mem_pool_init((uint64_t)TOTAL_MEM);
     /*初始化页表*/
-    uint32_t kernel_pages = get_kernel_pages((uint64_t)0xc0000000);
-    //page_table_init(kernel_pages);
+    uint32_t kernel_pages = get_kernel_pages((uint64_t)TOTAL_MEM);
+    page_table_init(kernel_pages);
+    put_str("    kernel PDE and PTE reload completion\n");
     put_str("memory initialization completion\n");
 }
